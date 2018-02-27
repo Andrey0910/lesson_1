513fca3d9d071b52e6f591d0a6b0bc082f9291bf
Andrey Petrov <petrovaa71@mail.ru>
2018-02-27 11:39:35 +0300

    Сделал 6 задание

---
+++ task_6/index.php
@@ -0,0 +1,21 @@
+<?php
+$bmw​с = ["model" => "X5", "speed" => 120, "doors" => 5, "year" => "2015"];
+$toyota = ["model" => "RAV4", "speed" => 110, "doors" => 5, "year" => "2016"];
+$opel = ["model" => "ASTRA", "speed" => 100, "doors" => 4, "year" => "2017"];
+
+$cars = [["name" => "bmw​с", "value" => $bmw​с],
+    ["name" => "toyota", "value" => $toyota],
+    ["name" => "opel", "value" => $opel]];
+
+foreach ($cars as $value)
+{
+    echo "CAR ".$value["name"];
+    echo "<br>";
+
+    foreach ($value["value"] as $item)
+    {
+        echo  $item." ";
+    }
+    echo "<br>";
+    echo "<br>";
+}
